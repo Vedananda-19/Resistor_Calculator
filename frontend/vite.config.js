daab0ae65ef0c0e4f61bb176c9f5e51cbd699af6
Vedananda-19 <pvedananda@gmail.com>
2026-05-17 02:57:12 +0530

    Deploying React

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-    plugins: [react(), svgr()]
+    plugins: [react(), svgr()],
+    base:"/Resistor_Calculator/"
 })
